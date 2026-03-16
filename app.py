5ade8736e49ed49e16906b632e47f149fc484d9a
urwithaaravp2-star <urwithaaravp2@gmail.com>
2026-03-16 14:34:59 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+print ("            THE CALCULATOR.                     ")
+a = float(input("Enter Your First number")
+b = float(input("Enter Your Second number")
+operation = input("Enter What u Wana do / + - × ")
+if operation = "+":
+print (a + b)
+elif operation = "×":
+ print (a * b)
+elif operation = "/":
+print (a / b)
+elif operation = "-":
+print (a - b)
+else :
+print("Invaild Format Entered by User")
 
