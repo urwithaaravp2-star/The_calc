8790d9afa1443462ae4ef7423c7c0e0580877bcf
urwithaaravp2-star <urwithaaravp2@gmail.com>
2026-03-27 18:59:00 +0530

    app.py
    More repeat

--- app.py
+++ app.py
@@ -51,4 +51,95 @@ elif operation = "-":
 print (a - b)
 else :
 print("Invaild Format Entered by User")
+a = float(input("Enter Your First number")
+b = float(input("Enter Your Second number")
+operation = input("Enter What u Wana do / + - × ")
+if operation = "+":
+print (a + b)
+elif operation = "×":
+ print (a * b)
+elif operation = "/":
+print (a / b)
+elif operation = "-":
+print (a - b)
+else :
+print("Invaild Format Entered by User")
+a = float(input("Enter Your First number")
+b = float(input("Enter Your Second number")
+operation = input("Enter What u Wana do / + - × ")
+if operation = "+":
+print (a + b)
+elif operation = "×":
+ print (a * b)
+elif operation = "/":
+print (a / b)
+elif operation = "-":
+print (a - b)
+else :
+print("Invaild Format Entered by User")
+a = float(input("Enter Your First number")
+b = float(input("Enter Your Second number")
+operation = input("Enter What u Wana do / + - × ")
+if operation = "+":
+print (a + b)
+elif operation = "×":
+ print (a * b)
+elif operation = "/":
+print (a / b)
+elif operation = "-":
+print (a - b)
+else :
+print("Invaild Format Entered by User")
+a = float(input("Enter Your First number")
+b = float(input("Enter Your Second number")
+operation = input("Enter What u Wana do / + - × ")
+if operation = "+":
+print (a + b)
+elif operation = "×":
+ print (a * b)
+elif operation = "/":
+print (a / b)
+elif operation = "-":
+print (a - b)
+else :
+print("Invaild Format Entered by User")
+a = float(input("Enter Your First number")
+b = float(input("Enter Your Second number")
+operation = input("Enter What u Wana do / + - × ")
+if operation = "+":
+print (a + b)
+elif operation = "×":
+ print (a * b)
+elif operation = "/":
+print (a / b)
+elif operation = "-":
+print (a - b)
+else :
+print("Invaild Format Entered by User")
+a = float(input("Enter Your First number")
+b = float(input("Enter Your Second number")
+operation = input("Enter What u Wana do / + - × ")
+if operation = "+":
+print (a + b)
+elif operation = "×":
+ print (a * b)
+elif operation = "/":
+print (a / b)
+elif operation = "-":
+print (a - b)
+else :
+print("Invaild Format Entered by User")
+a = float(input("Enter Your First number")
+b = float(input("Enter Your Second number")
+operation = input("Enter What u Wana do / + - × ")
+if operation = "+":
+print (a + b)
+elif operation = "×":
+ print (a * b)
+elif operation = "/":
+print (a / b)
+elif operation = "-":
+print (a - b)
+else :
+print("Invaild Format Entered by User")
 
