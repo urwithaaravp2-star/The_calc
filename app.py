26798802babce278ea592a8d4ab1f3565c5ab01b
urwithaaravp2-star <urwithaaravp2@gmail.com>
2026-03-27 18:57:21 +0530

    app.py
    Repeat function

--- app.py
+++ app.py
@@ -12,4 +12,43 @@ elif operation = "-":
 print (a - b)
 else :
 print("Invaild Format Entered by User")
+a = float(input("Enter Your First number")
+b = float(input("Enter Your Second number")
+operation = input("Enter What u Wana do / + - × ")
+if operation = "+":
+print (a + b)
+elif operation = "×":
+ print (a * b)
+elif operation = "/":
+print (a / b)
+elif operation = "-":
+print (a - b)
+else :
+print("Invaild Format Entered by User")
+a = float(input("Enter Your First number")
+b = float(input("Enter Your Second number")
+operation = input("Enter What u Wana do / + - × ")
+if operation = "+":
+print (a + b)
+elif operation = "×":
+ print (a * b)
+elif operation = "/":
+print (a / b)
+elif operation = "-":
+print (a - b)
+else :
+print("Invaild Format Entered by User")
+a = float(input("Enter Your First number")
+b = float(input("Enter Your Second number")
+operation = input("Enter What u Wana do / + - × ")
+if operation = "+":
+print (a + b)
+elif operation = "×":
+ print (a * b)
+elif operation = "/":
+print (a / b)
+elif operation = "-":
+print (a - b)
+else :
+print("Invaild Format Entered by User")
 
